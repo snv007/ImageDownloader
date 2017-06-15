4fe27c682e0b7409322a8a1becf8b4c0526d6bcc
Vivek S N <snv007@gmail.com>
2017-06-15 10:57:06 +0530

    Initial Commit

---
+++ ImageDownloader/SNVImageDownloader.framework/Headers/SNVImageDownloader.h
@@ -0,0 +1,19 @@
+//
+//  SNVImageDownloader.h
+//  SNVImageDownloader
+//
+//  Created by Vivek iLeaf on 6/14/17.
+//  Copyright © 2017 Vivek iLeaf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SNVImageDownloader.
+FOUNDATION_EXPORT double SNVImageDownloaderVersionNumber;
+
+//! Project version string for SNVImageDownloader.
+FOUNDATION_EXPORT const unsigned char SNVImageDownloaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SNVImageDownloader/PublicHeader.h>
+
+
